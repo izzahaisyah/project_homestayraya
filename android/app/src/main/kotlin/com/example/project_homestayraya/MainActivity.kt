56a890710131f7f5b47efc167603cbe55ebfaa9d
izzahaisyah <116508341+izzahaisyah@users.noreply.github.com>
2023-01-31 07:39:39 +0800

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/project_homestayraya/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.project_homestayraya
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/config.dart
@@ -0,0 +1,3 @@
+class CONFIG {
+  static const String server = "http://10.19.101.87/project_homestayraya";
+}
